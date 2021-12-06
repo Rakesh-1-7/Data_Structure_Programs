383ed1f84d4def266928c93c2a03f5f6dc6c6e84
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2021-12-06 23:44:00 +0530

    corrected program

--- CIRQ.C
+++ CIRQ.C
@@ -1,72 +1,71 @@
-#include <stdio.h>
-#include<conio.h>
-#include<process.h>
+#include<stdio.h>
+// #inlcude<conio.h>
 #define Q_SIZE 5
-
-int item, front = 0, rear = -1, q[10];
-
-void insertrear(int item, int *rear, int q[])
+int item,front = 0, rear = -1, q[Q_SIZE], count = 0;
+int deletefront();
+void insertrear()
 {
-  if(*rear == Q_SIZE - 1)
-  {
-    printf("Queue Overflow.\n");
-    return;
-  }
-  *rear+=1;
-  q[*rear] = item;
+	if(count == Q_SIZE)
+	{
+		printf("Queue Overflow\n");
+		return;
+	}	
+	rear = (rear+1)%Q_SIZE;
+	q[rear] = item;
+	count++;
 }
 
-int deletefront(int *front, int *rear, int q[])
+void displayQ()
 {
-  int x;
-  if(*front > *rear)
-    return -1;
-  return q[(*front)++];
-  //x = q[*front];
-  //*front += 1;
-  //return x;
+	int i,f;
+	if(count == 0)
+	{
+		printf("Queue is empty.\n");
+		return;
+	}
+	f = front;
+	printf("Contents of Queue : \n");
+	for(i=1;i<=count;i++)
+	{
+		printf("%d\n",q[f]);
+		f=(f+1)%Q_SIZE;
+	}
 }
 
-void displayQ(int front, int rear, int q[])
+int main()
 {
-  int i;
-  if(front > rear)
-  {
-    printf("Queue is empty.\n");
-    return;
-  }
-  printf("Contents of queue :\n");
-  for(i = front; i <= rear; i++)
-  printf("%d\n",q[i]);
+	int choice;
+	// clrscr();
+	for(;;)
+	{
+		printf("\n1Insert rear\n2.DeleteFront\n3.Display\n4.Exit\n");
+		printf("Enter the choice : ");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:printf("Enter the item to be inserted :");
+			       scanf("%d",&item);
+			       insertrear();
+			       break;
+			case 2:item = deletefront();
+			  	if(item == -1)
+				printf("Queue is empty.\n");
+				else
+				printf("Item deleted = %d\n",item);
+				break;
+			case 3:displayQ();
+				break;
+			default:return 0;
+		}
+	}
+
 }
 
-void main()
+int deletefront()
 {
-  int choice,item,front = 0, rear = -1, q[10];
-  clrscr();
-  for(;;)
-  {
-    printf("\n1.Insert Rear\n2.Delete Front\n3.Display\n4.Exit\n");
-    printf("Enter the choice : ");
-    scanf("%d",&choice);
-    switch(choice)
-    {
-      case 1:printf("Enter the item to be inserted : ");
-	     scanf("%d",&item);
-	     insertrear(item, &rear,q);
-	     break;
-      case 2:item = deletefront(&front, &rear, q);
-	     if(item==-1)
-	       printf("Queue s empty.\n");
-	     else
-	       printf("Item deleted = %d\n", item);
-	     break;
-      case 3: displayQ(front, rear, q);
-	      break;
-
-      default:exit(0);
-
-    }
-
-  }
+	if(count == 0)return -1;
+	item = q[front];
+	front = (front+1)%Q_SIZE;
+	count--;
+	return item;
 }
